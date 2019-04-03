1a37123f3772509587a0ec87f4922996eaf2043b
ISAAC HOFFMAN <trsileneh@gmail.com>
2019-04-03 09:01:57 -0700

    20190403
    final

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -1,85 +1,144 @@
 package blackjack;
 
-import static java.lang.System.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BlackJack{
-	//add in Player instance variable
-	Player player;
-	//add in Dealer instance variable
-	Dealer dealer;
+	
+	private ArrayList<Playerable> players;
 
 	public BlackJack()
 	{
-		player = new Player();
-		dealer = new Dealer();
+		Scanner scan = new Scanner(System.in);
+		System.out.print("enter the number of players: ");
+		int amt = scan.nextInt();
+		players = new ArrayList<Playerable>();
+		players.add(new Dealer());
+		for(int i = 0; i < amt; i++) {
+			players.add(new Player());
+		}
 	}
 
 	public void playGame()
 	{
 		Scanner scan = new Scanner(System.in);
-		char choice = 0;
 		boolean play = true;
 		
+		Dealer dealer = (Dealer) players.get(0);
+		
 		do {
-			player.resetHand();
-			dealer.resetHand();
+			// shuffle deck & reset hands
 			dealer.shuffle();
+			for(Playerable p : players) {
+				p.resetHand();
+			}
 			
-			player.addCardToHand(dealer.deal());
-			player.addCardToHand(dealer.deal());
+			// deal 2 cards
+			for(int i = players.size()-1; i >= 0; i--) {
+				players.get(i).addCardToHand(dealer.deal());
+				players.get(i).addCardToHand(dealer.deal());
+			}
 			
-			dealer.addCardToHand(dealer.deal());
-			dealer.addCardToHand(dealer.deal());
-			
-			System.out.println("PLAYER");
-			print(player);
-			while(player.getHandValue() < 21 && player.hit()) {
-				player.addCardToHand(dealer.deal());
+			// player's turns
+			for(int i = 1; i < players.size(); i++) {
+				Player player = (Player) players.get(i);
+				System.out.println("PLAYER " + i);
 				print(player);
-				
+				while(player.getHandValue() < 21 && player.hit()) {
+					player.addCardToHand(dealer.deal());
+					print(player);
+				}
+				System.out.println();
 			}
+			
+			// dealer's turn
 			System.out.println("DEALER");
-			print(dealer);
-			while(dealer.hit()) {
-				dealer.addCardToHand(dealer.deal());
-				print(dealer);
+			print(players.get(0));
+			while(players.get(0).hit()) {
+				players.get(0).addCardToHand(dealer.deal());
+				print(players.get(0));
 			}
 			
+			System.out.println();
 			
-			if(dealer.getHandValue() > 21) {
-				System.out.println("player wins - dealer busted!");
-				player.setWinCount(player.getWinCount()+1);
-			}else if(player.getHandValue() > 21) {
-				System.out.println("dealer wins - player busted!");
-				dealer.setWinCount(dealer.getWinCount()+1);
-			}else if(player.getHandValue() > dealer.getHandValue()) {
-				System.out.println("player has bigger hand value!");
-				player.setWinCount(player.getWinCount()+1);
-			}else if(player.getHandValue() < dealer.getHandValue()) {
-				System.out.println("dealer has bigger hand value!");
-				dealer.setWinCount(dealer.getWinCount()+1);
-			}else if(player.getHandValue() == dealer.getHandValue()) {
-				System.out.println("hand values are equal! it's a tie!");
+			// check who won & update win total
+			ArrayList<Integer> possibleWinners = new ArrayList<Integer>();
+			for(Playerable p : players) {
+				if(p.getHandValue() <= 21) {
+					possibleWinners.add(players.indexOf(p));
+				}
+			}
+			// if all busted except one
+			if(possibleWinners.size() == 1) {
+				if(possibleWinners.get(0) == 0) {
+					System.out.println("dealer won! all other players busted.");
+					players.get(0).setWinCount(players.get(0).getWinCount() + 1);
+				}
+				else {
+					System.out.println("player " + possibleWinners.get(0) + " won! all other players busted.");
+					players.get(possibleWinners.get(0)).setWinCount(players.get(possibleWinners.get(0)).getWinCount() + 1);
+				}
+			}
+			// if all busted
+			else if(possibleWinners.size() == 0) {
+				System.out.println("tie - all players busted!");
+			}
+			// else
+			else if(possibleWinners.size() > 1) {
+				int maxInd = 0;
+				int maxHand = players.get(0).getHandValue();
+				boolean allEqual = allEqual(possibleWinners, players);
+				for(int i : possibleWinners) {
+					if(players.get(i).getHandValue() > maxHand) {
+						maxHand = players.get(i).getHandValue();
+						maxInd = i;
+					}
+				}
+				if(allEqual) {
+					System.out.println("tie - all non-busted players have the same hand value!");
+				}
+				else if(maxInd == 0) {
+					System.out.println("dealer won! (largest hand value)");
+					players.get(0).setWinCount(players.get(0).getWinCount() + 1);
+				}else {
+					System.out.println("player " + maxInd + " won! (largest hand value)");
+					players.get(maxInd).setWinCount(players.get(maxInd).getWinCount() + 1);
+				}
 			}
 			
-			System.out.println("dealer has won " + dealer.getWinCount() + " times.");
-			System.out.println("player has won " + player.getWinCount() + " times.");
+			// print win statement
+			System.out.println("dealer has won " + players.get(0).getWinCount() + " times.");
+			for(int i = 1; i < players.size(); i++) {
+				System.out.println("player " + i + " has won " + players.get(i).getWinCount() + " times.");
+			}
+			
+			System.out.println();
 			
 			System.out.print("would you like to play again? [y/n] -- ");
 			String ans = scan.nextLine();
 			if(!ans.equalsIgnoreCase("y")) {
 				play = false;
 			}
+			System.out.println();
 			
 		}while(play);
 
 	}
 	
-	public void print(AbstractPlayer player) {
+	private void print(Playerable player) {
 		System.out.println("hand value: " + player.getHandValue());
 		System.out.println("hand size: " + player.getHandSize());
 		System.out.println("cards in hand: " + player);
+	}
+	
+	private boolean allEqual(ArrayList<Integer> pw, ArrayList<Playerable> players) {
+		int hand = players.get(0).getHandValue();
+		for(int i : pw) {
+			if(players.get(i).getHandValue() != hand) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	public static void main(String[] args)
